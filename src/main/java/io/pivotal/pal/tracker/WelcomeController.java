59a1e1308dde6e6bee90f5d40da45a8b134d3540
Saumya Sara Raju (Digital) <SA20089518@wipro.com>
2019-06-25 17:37:09 +0530

    git chng

---
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -0,0 +1,26 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class WelcomeController {
+
+    @Value("${welcome.message}")
+    String message;
+
+    public WelcomeController() {
+    }
+
+    public  WelcomeController(String message) {
+        this.message = message;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return message;
+    }
+}

cfc6b98bcafe098b6630b8523567419b59a8890a
Saumya Sara Raju (Digital) <SA20089518@wipro.com>
2019-06-26 13:07:15 +0530

    TimeEntry

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -4,23 +4,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 public class WelcomeController {
 
-    @Value("${welcome.message}")
-    String message;
+    private String welcomeMessage;
 
-    public WelcomeController() {
-    }
-
-    public  WelcomeController(String message) {
-        this.message = message;
+    public WelcomeController(
+            @Value("${welcome.message}") String welcomeMessage
+    ) {
+        this.welcomeMessage = welcomeMessage;
     }
 
     @GetMapping("/")
     public String sayHello() {
-        return message;
+        return welcomeMessage;
     }
 }
